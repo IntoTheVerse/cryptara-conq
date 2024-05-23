043f6f8627208b452370edd9d32181021e804351
devELIOper <lopeselio@gmail.com>
2024-05-23 17:23:13 +0530

    added App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Preloader from './components/Preloader.js';
+import Landing from './components/Landing.js';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate a loading time with a timeout
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 5000); // 5000 ms = 5 seconds
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isLoading ? <Preloader /> : <Landing />}
     </div>
   );
 }
