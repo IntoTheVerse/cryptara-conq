c1543d11980cf19ea53760389c0089ef3bae379f
devELIOper <lopeselio@gmail.com>
2024-05-23 20:19:34 +0530

    added background andd preloader text align

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,12 +1,23 @@
 import React, { useEffect } from 'react';
+import '../index.css'
 
 
 const Landing = () => {
 
 
     return (
-        <div className=''>
-            GM Cryptara Conquest
+        <div className='dungeon overflow-hidden cursor-sword flex-row'>
+            <div className='dungeon relative top-0 left-0 h-lvh bg-cover bg-dungeonScene font-pixeled text-white-lace relative z-0'>
+                <div className='droplets relative top-0 z-15 h-full w-lvw'>
+                    <div className='droplets inset-x-0 top-1 h-lvh'>
+                        <div className='absolute drip1'></div>
+                        <div className='absolute drip2'></div>
+                        <div className='absolute drip3'></div>
+                        <div className='absolute drip4'></div>
+
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Preloader.js
+++ src/components/Preloader.js
@@ -3,8 +3,8 @@ import skull from '../assets/logo.png';
 
 const Preloader = () => {
     return(
-        <div className='relative w-screen h-screen flex flex-col justify-center items-center'>
-            <div className='font-pixeled xs:text-2xl lg:text-6xl xl:text-7xl mobile-landscape:text-4xl text-white-lace absolute text-shadow-pixel-neon tracking-[8px] xs:top-[35vmax] lg:top-[30vmax] xl:top-[25vmin] mobile-landscape:top-[20vmin]' data-text='U'>
+        <div className='relative w-screen h-screen flex flex-col justify-center items-center cursor-sword'>
+            <div className='font-pixeled xs:text-2xl lg:text-6xl xl:text-7xl mobile-landscape:text-4xl text-white-lace text-center absolute text-shadow-pixel-neon tracking-[8px] xs:top-[35vmax] lg:top-[30vmax] xl:top-[25vmin] mobile-landscape:top-[20vmin]' data-text='U'>
                 Taking &nbsp;
                 <span className='animate-flicker-slow'>
                 you &nbsp;
