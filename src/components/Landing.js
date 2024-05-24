e558d0a7c864d994e0c4bac37757739ddc53d7f1
devELIOper <lopeselio@gmail.com>
2024-05-24 16:03:41 +0530

    added shrine and gifs

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,28 +1,47 @@
 import React, { useEffect } from 'react';
 import '../index.css'
-import { Tooltip, initTWE } from "tw-elements";
+import Carousel from './Carousel.js'
 
+const slides = [
+    "https://i.ibb.co/ZMJvCgS/scietist2.gif",
+    "https://i.ibb.co/7ybFJSf/steampunk2.gif",
+    "https://i.ibb.co/Snk95LY/thief2.gif"
+]
+
+const names = [
+    "Dr. Neutron Spark",
+    "Gearlock",
+    "Shadowstrike"
+]
 
 const Landing = () => {
     useEffect(() => {
-        initTWE({ Tooltip });
-      }, []);
+
+    }, []);
 
 
     return (
-        <div className='dungeon overflow-hidden cursor-sword flex-row'>
+        <div className='dungeon flex overflow-hidden cursor-sword flex-row'>    
             <div className='dungeon flex-row relative top-0 left-0 h-dvh bg-cover bg-dungeonScene font-pixeled text-white-lace relative z-0'>
-                <div className='shrine flex-col-reverse justify-items-center relative bottom-0 left-0 h-1/2 w-1/2 z-1'>
-                </div>
-                <div className='shrine flex justify-around float-left relative bottom-0 left-0 h-1/2 w-1/2 z-1'>
-                    <div className='flex flex-row justify-center items-center bg-shrine bg-contain bg-no-repeat h-3/4 w-1/2 bottom-0 relative z-2' />
-                </div>
-                <div className='droplets relative top-0 z-15 h-full w-lvw'>
+                <div className='dungeon droplets flex relative top-0 z-1 h-full w-dvw'>
                     <div className='droplets inset-x-0 top-1 h-lvh'>
                         <div className='absolute drip1'></div>
                         <div className='absolute drip2'></div>
                         <div className='absolute drip3'></div>
                         <div className='absolute drip4'></div>
+                    </div>
+                </div>
+                <div className='shrine flex-col-reverse justify-items-center relative bottom-0 left-0 h-1/2 w-1/2 float-left'>
+                </div>
+                <div className='shrine flex justify-around float-left absolute bottom-0 left-0 h-1/2 w-1/2 z-2 xs:hidden xl:flex'>
+                    <div className='flex flex-row justify-center items-center bg-shrine bg-contain bg-no-repeat h-3/4 w-1/2 bottom-0 relative z-2'>
+                        <div className='max-w-lg flex flex-row justify-center items-center object-contain z-2 size-full h-full w-1/2 px-6'>
+                            <Carousel className="z-2 h-3/4" autoSlide={true}>
+                                {slides.map((s) => (
+                                    <img src={s} />
+                                ))}
+                            </Carousel>
+                        </div>  
                     </div>
                 </div>
             </div>
