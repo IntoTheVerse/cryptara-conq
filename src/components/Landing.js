8bbdde5a95d0fd62d42a6d1e51187e08a437f953
devELIOper <lopeselio@gmail.com>
2024-05-26 02:59:26 +0530

    fixed mobile css bug

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -50,18 +50,18 @@ const Landing = () => {
                     </div>
                 </div>
                 <div className='lg:flex lg:w-[50vw] xs:w-screen h-screen absolute flex-col justify-center items-center z-2 top-0 lg:right-0'>
-                    <div className='flex flex-col justify-center items-center lg:space-y-4 xs:space-y-2 lg:mt-[20vh] xs:mt-[20vh] text-white-lace xs:border-amber-400 xs:border-4 lg:border-none bg-zinc-900/75 rounded-lg lg:w-1/2 lg:h-1/2 xs:w-3/4 xs:h-1/2 xs:mx-auto z-2 justify-center items-center align-middle'>
+                    <div className='flex flex-col justify-center items-center lg:space-y-4 xs:space-y-2 lg:mt-[20vh] xs:mt-[30vh] text-white-lace xs:border-amber-400 xs:border-4 lg:border-none bg-zinc-900/75 rounded-lg lg:w-1/2 lg:h-1/2 xs:w-3/4 xs:h-1/2 xs:mx-auto z-2 justify-center items-center align-middle'>
                         <div className='lg:hidden xl:hidden flex flex-col space-y-2 mx-[0.6rem] mt-1 text-white-lace w-3/4 h-3/4 z-2 justify-center items-center'>
-                            <div className='bg-logo bg-contain bg-center bg-no-repeat h-3/4 w-full z-2 mt-1 mx-auto items-center px-auto align-middle'></div>
+                            <div className='bg-logo bg-contain bg-center bg-no-repeat h-[15vh] w-full z-2 mt-1 mx-auto items-center px-auto align-middle'></div>
                             <div className='font-pixeled mt-0 text-white-lace text-center text-[0.6rem] w-full mx-1 z-2 rounded-full'><p><span className='inline-block align-middle'>An epic 2D survival RPG coming soon on Eclipse! &nbsp;<img className='bg-slate-950 rounded-full w-5 h-5 self-center inline-block align-middle' src={Eclipse}/></span></p></div>
                         </div>
                         <div className='font-pixeled lg:text-xs xs:text-[0.4rem] xs:text-center lg:mt-2 xs:mt-1 xs:mb-1 text-white-lace lg:w-3/4 xs:w-3/4 xs:h-1/4 z-2 xs:mx-auto'><p><span className='inline-block align-middle'>Built by IntoTheVerse Inc. &nbsp;<img className='xs:w-5 xs:h-5 lg:w-10 lg:h-10 self-center inline-block align-middle' src={ITV}/></span></p></div>
-                            <a className='bg-twitter bg-contain bg-no-repeat p-3 z-2 lg:h-1/4 lg:w-1/2 xs:w-48 xs:h-24 xs:ml-[15px] xs:align-middle xs:flex xs:items-center xs:justify-center' href="https://x.com/CryptaraConq"><div></div></a>
-                            <a className='bg-discord bg-contain bg-no-repeat p-3 z-2 lg:h-1/4 lg:w-1/2 xs:w-48 xs:h-24 xs:ml-[15px] xs:align-middle xs:flex xs:items-center xs:justify-center' href="https://discord.gg/w94EVQRPRf"><div></div></a>
-                            <a className='bg-whitepaper bg-contain bg-no-repeat p-3 z-2 lg:h-1/4 lg:w-1/2 xs:w-48 xs:h-24 xs:ml-[15px] xs:align-middle xs:flex xs:items-center xs:justify-center' href="https://wiki.cryptara.world/"><div></div></a>
-                            <a href="https://rarible.com/" className='lg:hidden xl:hidden xs:bg-rarible xs:bg-contain xs:bg-no-repeat xs:p-3 xs:z-2 xs:h-1/4 xs:w-3/4'><div></div></a>
+                            <a className='bg-twitter bg-contain bg-no-repeat bg-center p-3 z-2 lg:h-1/4 lg:w-1/2 xs:w-full xs:h-[5vh] xs:m-auto xs:align-middle xs:flex xs:items-center xs:justify-center xs:item-center' href="https://x.com/CryptaraConq"><div></div></a>
+                            <a className='bg-discord bg-contain bg-no-repeat bg-center p-3 z-2 lg:h-1/4 lg:w-1/2 xs:w-full xs:h-[5vh] xs:m-auto xs:align-middle xs:flex xs:items-center xs:justify-center xs:item-center' href="https://discord.gg/w94EVQRPRf"><div></div></a>
+                            <a className='bg-whitepaper bg-contain bg-no-repeat bg-center p-3 z-2 lg:h-1/4 lg:w-1/2 xs:w-full xs:h-[5vh] xs:m-auto xs:align-middle xs:flex xs:items-center xs:justify-center xs:item-center' href="https://wiki.cryptara.world/"><div></div></a>
+                            <a href="https://rarible.com/" className='lg:hidden xl:hidden xs:bg-rarible xs:bg-contain bg-center xs:bg-no-repeat xs:p-3 xs:z-2 xs:h-[15vh] xs:w-full'><div></div></a>
                         </div>
-                    <a href="https://rarible.com/" className='bg-rarible bg-contain bg-no-repeat p-3 z-2 h-1/4 w-3/4'><div></div></a>
+                    <a href="https://rarible.com/" className='bg-rarible bg-contain bg-center bg-no-repeat p-3 z-2 h-1/4 w-3/4'><div></div></a>
                 </div>
                 {/* </div> */}
                 {/* <div className='lg:hidden xl:hidden mx-[25vw] size-full shrine flex-1 absolute bottom-0 inset-x-0 h-64 w-screen item-center justify-center items-center w-full z-2'>
